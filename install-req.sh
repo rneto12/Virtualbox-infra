45ba9abb8baadbd5465a5ad352c67dd93cf69c24
Renato <rneto12@gmail.com>
2021-09-27 22:44:01 -0400

    Update install-req.sh

--- install-req.sh
+++ install-req.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Script em construcao
+#Run this script as sudo
 
 # add hashicorp repo
 apt update
@@ -10,8 +10,6 @@ apt-add-repository -y "deb [arch=$(dpkg --print-architecture)] https://apt.relea
 
 # install packages
 apt update
-apt install -y terraform=0.12.31 ansible packer virtualbox
+apt install -y terraform=0.12.31 ansible packer virtualbox xorriso
 echo virtualbox-ext-pack virtualbox-ext-pack/license select true | debconf-set-selections | apt install virtualbox-ext-pack -y
 apt-mark hold terraform
-
-
